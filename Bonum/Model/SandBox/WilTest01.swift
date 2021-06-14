6a201ac4bc9ad41ced179480791b84ad7a16d6da
rwilf <wroguet@gmail.com>
2021-06-14 12:18:59 +0200

    Add Bonus on main

---
+++ Bonum/Model/SandBox/WilTest01.swift
@@ -0,0 +1,20 @@
+//
+//  WilTest01.swift
+//  Bonum
+//
+//  Created by Wilfried Roguet on 11/06/2021.
+//
+
+import SwiftUI
+
+struct WilTest01: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WilTest01_Previews: PreviewProvider {
+    static var previews: some View {
+        WilTest01()
+    }
+}
