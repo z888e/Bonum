19227c0746b740478c874fe345978fa1d4cf7536
zoé <24644446+z888e@users.noreply.github.com>
2021-06-15 11:17:36 +0200

    Ajout de 2 fichiers sous-vues du Journal pour valider l'affichage des données Santé

---
+++ Bonum/View/DiaryList.swift
@@ -0,0 +1,20 @@
+//
+//  DiaryList.swift
+//  Bonum
+//
+//  Created by Zoé Hartman on 15/06/2021.
+//
+
+import SwiftUI
+
+struct DiaryList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiaryList_Previews: PreviewProvider {
+    static var previews: some View {
+        DiaryList()
+    }
+}
