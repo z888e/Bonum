66dfdb116563c7dd894e8357e68f4d8b2b6dfd8b
rwilf <wroguet@gmail.com>
2021-06-16 17:40:46 +0200

    Notification updates

--- Bonum/BonumApp.swift
+++ Bonum/BonumApp.swift
@@ -15,6 +15,7 @@ struct BonumApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView().environmentObject(healthStore)
+//            UserNotifications()
         }
     }
 }
