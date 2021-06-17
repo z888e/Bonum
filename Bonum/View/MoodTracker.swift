d70028f0f8cee5d6f0245af333f1bcb00441dba6
zoé <24644446+z888e@users.noreply.github.com>
2021-06-17 22:37:35 +0200

    Test MoodTracker

--- Bonum/View/MoodTracker.swift
+++ Bonum/View/MoodTracker.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct MoodTracker: View {
+    
+    @EnvironmentObject var userData:UserData
+    @State private var scoreEntered : Double = 5
+    
     var body: some View {
-        Text("Le MoodTracker")
+        
+        VStack(alignment: .leading) {
+            
+            Slider(value: $scoreEntered, in: 0...10).padding()
+            //TODO : valeurs/sensibilite du slider, ou stepper avec style custom?
+            
+            Button(action: {
+                //TODO:append le mood avec la date et le type
+            }, label: {
+                //TODO: texte
+                Text("Valider")
+            })
+            
+            //uniquement pour verifier que le JSON est chargé
+            Text("Historique des Notes").padding()
+
+            List {
+                ForEach(userData.userMoodHistory, id: \.self) { moodEntry in
+                    
+                    let entry = String(moodEntry.rating ?? 0)
+                    Text(entry)
+                    
+                }
+            }.padding()
+            .listStyle(PlainListStyle())
+            
+        }
+//        .navigationTitle("Parcours")
+//        .navigationBarItems(trailing:
+//                                NavigationLink("Nouveau jalon", destination: Text("Ajout d'un nouveau jalon"))
+//        )
+        
     }
 }
 
@@ -18,3 +53,6 @@ struct MoodTracker_Previews: PreviewProvider {
         MoodTracker()
     }
 }
+
+
+
