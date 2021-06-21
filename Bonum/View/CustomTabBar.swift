9459e1cc21f6dad344f2eb1bff3a6ac44e0181a2
rwilf <wroguet@gmail.com>
2021-06-21 09:09:54 +0200

    Custom medium widget and notification

--- Bonum/View/CustomTabBar.swift
+++ Bonum/View/CustomTabBar.swift
@@ -15,25 +15,32 @@ struct CustomTabBar: View {
     let iconSize: CGFloat = 20
     let unselectetIconColor = Color.gray
     let selectetIconColor = Color.orange
-
+    
+    @AppStorage("lastMoodDate") private var lastMoodDate: Date = Date()
+    
+    let durations: [Double] = [5, 2, 1, 0.6, 0.3]
+    let timer = Timer.publish(every: 230, on: .main, in: .common).autoconnect()
+    @State private var counter: Int = 3
+    
+    @State private var sinceLastMoodDate : Double = 1
     
     var body: some View {
         
         VStack{
             ZStack{
-            
-            switch tabIndex {
-            case 0:
-                Diary()
-            case 1:
-                MoodTracker()
-            case 2:
-                Journey()
-            default:
-                MoodTracker()
+                
+                switch tabIndex {
+                case 0:
+                    Diary()
+                case 1:
+                    MoodTracker()
+                case 2:
+                    Journey()
+                default:
+                    MoodTracker()
+                }
             }
-            }
-            .padding(.bottom, -24) // Je ne sais pas pq cette valeur marche...
+            .padding(.bottom, -8)  // NE MARCHE pas ds tous les cas
             
             
             HStack{
@@ -54,37 +61,46 @@ struct CustomTabBar: View {
                 Button(action: {
                     self.tabIndex = 1
                 }) {
-                    //            Image(systemName: "plus.circle")
-                    PulsingButton(colorB: (self.tabIndex == 1 ? selectetIconColor : unselectetIconColor), sizeB: iconSize*3, minimumRatioB: 0.5, durationB: 0.5)
+                    PulsingButton(colorB: (self.tabIndex == 1 ? selectetIconColor : unselectetIconColor), sizeB: iconSize*4, minimumRatioB: 0.5, durationB: durations[counter])
+                        .onReceive(timer) { time in
+                            if self.counter == 4 {
+                                self.timer.upstream.connect().cancel()
+                            } else {
+                                self.counter += 1
+                            }
+                        } // permet d'accélérer la pulsation... pb : il faudrait aussi réinitialiser l'animation avant
                 }
-                .offset(y: -iconSize*1.5)
-                
-                Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
-                
-                Button(action: {
-                    self.tabIndex = 2
-                }) {
-                    VStack{
-                        Image(systemName: "arrow.triangle.pull")
-                            .font(.system(size: iconSize))
-                        Text("Parcours")
-                            .font(.system(size: fontSize))
-                    }
-                }
-                .foregroundColor(self.tabIndex == 2 ? selectetIconColor : unselectetIconColor)
-                
-            } // fin HStack
-            .padding(.horizontal, 50.0)
-            .background(Color.black.opacity(0.05))
-            
-        } // fin VStack
-        .edgesIgnoringSafeArea(.bottom)
+                .offset(y: -iconSize*2)
+
         
-    }
+        Spacer(minLength: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/)
+        
+        Button(action: {
+            self.tabIndex = 2
+        }) {
+            VStack{
+                Image(systemName: "arrow.triangle.pull")
+                    .font(.system(size: iconSize))
+                Text("Parcours")
+                    .font(.system(size: fontSize))
+                //                        Text(String(sinceLastMoodDate))
+            }
+        }
+        .foregroundColor(self.tabIndex == 2 ? selectetIconColor : unselectetIconColor)
+        
+    } // fin HStack
+    .padding(.horizontal, 50.0)
+    .background(Color.black.opacity(0.05))
+    
+} // fin VStack
+.edgesIgnoringSafeArea(.bottom)
+
+}
 }
 
 struct CustomTabBar_Previews: PreviewProvider {
     static var previews: some View {
         CustomTabBar(tabIndex: .constant(1))
+            .environmentObject(UserData(name: "Albert", userElementsList: MYELEMENTS, userJourneyEvents: MYJOURNEY, userMoodHistory: MYMOODS))
     }
 }
--- Bonum/View/PulsingButton.swift
+++ Bonum/View/PulsingButton.swift
@@ -18,33 +18,31 @@ struct PulsingButton: View {
     
     var body: some View {
         
-        ZStack{
-            Circle().fill(colorB.opacity(0.25)).frame(width: sizeB, height: sizeB).scaleEffect(self.animate ? 1 : minimumRatioB)
-            
-            Circle().fill(colorB.opacity(0.35)).frame(width: sizeB*0.75, height: sizeB*0.75).scaleEffect(self.animate ? 1 : minimumRatioB)
-            
-            Circle().fill(colorB.opacity(0.45)).frame(width: sizeB*0.5, height: sizeB*0.5).scaleEffect(self.animate ? 1 : minimumRatioB)
-            
-            Circle().fill(Color.white).frame(width: sizeB*0.4, height: sizeB*0.4)
-            
-            Image(systemName: "plus.circle.fill")
-                .font(.system(size: sizeB*0.5))
-                .foregroundColor(colorB)
-//                .rotationEffect(.init(degrees: animate ? -10 : 10))
- 
-//            Circle().fill(colorB.opacity(1)).frame(width: sizeB*0.2, height: sizeB*0.2).scaleEffect(self.animate ? minimumRatioB : 1)
-        }
-        .onAppear{
-            self.animate.toggle()
-        }
-        .animation(Animation.linear(duration: durationB).repeatForever(autoreverses: true))
-
+            ZStack{
+                Circle().fill(colorB.opacity(0.25)).frame(width: sizeB, height: sizeB).scaleEffect(self.animate ? 1 : minimumRatioB)
+                
+                Circle().fill(colorB.opacity(0.35)).frame(width: sizeB*0.75, height: sizeB*0.75).scaleEffect(self.animate ? 1 : minimumRatioB)
+                
+                Circle().fill(colorB.opacity(0.45)).frame(width: sizeB*0.5, height: sizeB*0.5).scaleEffect(self.animate ? 1 : minimumRatioB)
+                
+                Circle().fill(Color.white).frame(width: sizeB*0.4, height: sizeB*0.4)
+                
+                Image(systemName: "plus.circle.fill")
+                    .font(.system(size: sizeB*0.5))
+                    .foregroundColor(colorB)
+                
+                //            Circle().fill(colorB.opacity(1)).frame(width: sizeB*0.2, height: sizeB*0.2).scaleEffect(self.animate ? minimumRatioB : 1)
+            }
+            .onAppear{
+                self.animate.toggle()
+            }
+            .animation(Animation.linear(duration: durationB).repeatForever(autoreverses: true))
         
     }
 }
 
 struct PulsingButton_Previews: PreviewProvider {
     static var previews: some View {
-        PulsingButton(colorB: .orange, sizeB: 100, minimumRatioB: 0.5, durationB: 0.5)
+        PulsingButton(colorB: .orange, sizeB: 100, minimumRatioB: 0.5, durationB: 0.3)
     }
 }
